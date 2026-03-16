3e9cfb347bf841e6304e2ac1ae37db796d187055
zaryabzip <110787259+zaryabzip@users.noreply.github.com>
2026-03-17 00:01:57 +0500

    hascity test (should fail)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(calgary));
+    }
 }
